db565d05fc8e7570b54179f34165a68f485914aa
Theocode12 <vchidubem9@gmail.com>
2022-06-01 06:33:29 +0100

    check if linked list is circula

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <stdio.h>
 
 /**
  * check_cycle - checks for linked list cycle
@@ -8,57 +9,18 @@
 
 int check_cycle(listint_t *list)
 {
-	int i, j, size = 10, nw_size;
-	listint_t **node_add;
+	int i, j;
+	listint_t *node_adr[8];
 
-	node_add = malloc(sizeof(listint_t *) * size);
 	for (i = 0; list; i++)
 	{
-		node_add[i] = list;
+		node_adr[i] = list;
 		for (j = 0; j < i; j++)
 		{
-			if (node_add[j] == list)
-			{
-				free(node_add);
+			if (node_adr[j] == list)
 				return (1);
-			}
 		}
 		list = list->next;
-		if (i >= size - 1)
-		{
-			nw_size = size * 2;
-			node_add = _realloc(node_add, size, nw_size);
-			size = nw_size;
-		}
 	}
-	free(node_add);
 	return (0);
-}
-
-/**
- * _realloc - replicate realloc on stdlib
- * @ptr: pointer to original memory
- * @old_size: size of previous memory
- * @new_size: size of new memory
- * Return: returns pointer to new memory
- */
-
-void *_realloc(listint_t **ptr, int old_size, int new_size)
-{
-	int i;
-	listint_t **str, **old_ptr;
-
-	old_ptr = ptr;
-	str = malloc(new_size);
-	if (!str)
-	{
-		free(ptr);
-		return (NULL);
-	}
-	for (i = 0; i < (old_size || new_size--); i++)
-	{
-		str[i] = old_ptr[i];
-	}
-	free(ptr);
-	return (str);
 }
