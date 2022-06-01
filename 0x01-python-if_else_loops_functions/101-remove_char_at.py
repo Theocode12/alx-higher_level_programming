e560318459656fa7b8059420818b9e01b045fd8e
Theocode12 <vchidubem9@gmail.com>
2022-06-01 05:26:54 +0100

    removes char at a particular index

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if len(str) > n and n < 0:
+    if len(str) > n and n > 0:
         cp_str = str[:n] + str[n + 1:]
         return cp_str
     return str
