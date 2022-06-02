7a90270d00bde414623760b00e986b4c8e61b857
Theocode12 <vchidubem9@gmail.com>
2022-06-02 21:55:32 +0100

    calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+import calculator_1 as cal
+if __name__ == '__main__':
+    argv = sys.argv[1:]
+    if len(argv) != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if argv[1] == '+':
+        print("{} + {} = {}".format(argv[0], argv[2], cal.add(int(argv[0]), int(argv[2]))))
+    elif argv[1] == '-':
+        print("{} - {} = {}".format(argv[0], argv[2], cal.sub(int(argv[0]), int(argv[2]))))
+    elif argv[1] == '*':
+        print("{} * {} = {}".format(argv[0], argv[2], cal.mul(int(argv[0]), int(argv[2]))))
+    elif argv[1] == '/':
+        print("{} / {} = {}".format(argv[0], argv[2], cal.div(int(argv[0]), int(argv[2]))))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
