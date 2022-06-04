1e0c858b1c6cf352478d5cf35bec98328e5e57fb
Theocode12 <vchidubem9@gmail.com>
2022-06-04 19:23:21 +0100

    tuple update

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,10 @@ def add_tuple(tuple_a=(0, 0), tuple_b=(0, 0)):
         tuple_a = tuple_a[:2]
     if len(tuple_b) >= 2:
         tuple_b = tuple_b[:2]
-    if len(tuple_a) == 0 or len(tuple_b) == 0:
-        return tuple_a + tuple_b
+    if len(tuple_a) == 0:
+        return tuple_b
+    if len(tuple_b) == 0:
+        return tuple_a
     if len(tuple_a) == 1:
         tuple_a = tuple_a + (0,)
     if len(tuple_b) == 1:
@@ -13,5 +15,4 @@ def add_tuple(tuple_a=(0, 0), tuple_b=(0, 0)):
     nw_tup = []
     for i in range(2):
         nw_tup.append(tuple_a[i] + tuple_b[i])
-    nw_tup = tuple(nw_tup)
-    return nw_tup
+    return tuple(nw_tup)
