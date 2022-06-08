2fa507b5148acad48055fe9dcc68c37b300d8b01
Theocode12 <vchidubem9@gmail.com>
2022-06-08 20:16:15 +0100

    find best score in dictionary

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary == None:
+        return None
+    best_key = ""
+    best_value = list(a_dictionary.values())[0]
+    for key, value in a_dictionary.items():
+        if  best_value <= value:
+            best_key = key
+            best_value = value
+    return best_key
