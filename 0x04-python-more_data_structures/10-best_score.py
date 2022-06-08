38bc4d96c5306688f47b1b275436b450ed3650db
Theocode12 <vchidubem9@gmail.com>
2022-06-08 20:34:29 +0100

    find best score in dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@ def best_score(a_dictionary):
     best_key = ""
     best_value = list(a_dictionary.values())[0]
     for key, value in a_dictionary.items():
-        if best_value < value:
-            best_value = value
+        if best_value <= value:
             best_key = key
+            best_value = value
     return best_key
