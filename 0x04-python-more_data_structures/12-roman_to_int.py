88899709fb335dd0b042afd34640e483831fa2e2
Theocode12 <vchidubem9@gmail.com>
2022-06-09 03:35:02 +0100

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string: "str") -> "int":
+    rm_sym = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rm_str = roman_string
+    roman_int = 0
+    flag = True
+    if roman_string:
+        for i in range(len(roman_string)):
+            if roman_string[i] in rm_sym:
+                roman_int += rm_sym.get(roman_string[i])
+            else:
+                return
+            if i > 0 and flag:
+                if (rm_str[i] == 'V' or rm_str[i] == 'X') \
+                        and rm_str[i - 1] == 'I':
+                    roman_int -= 2
+                    flag = False
+                    continue
+                elif (rm_str[i] == 'L' or rm_str[i] == 'C') \
+                        and rm_str[i - 1] == 'X':
+                    roman_int -= 20
+                    flag = False
+                    continue
+                elif (rm_str[i] == 'D' or rm_str[i] == 'M') \
+                        and rm_str[i - 1] == 'C':
+                    roman_int -= 200
+                    flag = False
+                    continue
+            flag = True
+        return roman_int
+    return
