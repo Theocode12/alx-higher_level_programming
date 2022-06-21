4bd3ba4859240e1d937a0ee242c378cb015020e4
Theocode12 <vchidubem9@gmail.com>
2022-06-21 23:06:14 +0100

    set restriction in init method

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module demonstrates the use of classes by
+creating a square class
+"""
+
+
+class Square(object):
+    """A class use to represent a square Object"""
+    def __init__(self, size=0):
+        """Initialize a square object"""
+        if (isinstance(size, int)) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
