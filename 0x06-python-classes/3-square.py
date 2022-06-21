bfb732c031fbbd9656f02755693ff8d4027d3a90
Theocode12 <vchidubem9@gmail.com>
2022-06-21 23:17:13 +0100

    getting the area of a square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module demonstrates the use of classes by
+creating a square class
+"""
+
+
+class Square(object):
+    """A class use to represent a square Object"""
+
+    def __init__(self, size=0):
+        """Initialize a square object"""
+
+        if (isinstance(size, int)) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = size
+
+    def area(self):
+        """Return the area of this square"""
+
+        return self._Square__size ** 2
