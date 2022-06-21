973e3979abd9e1965f1b8b5780e2545452416b58
Theocode12 <vchidubem9@gmail.com>
2022-06-21 22:37:02 +0100

    creating an instance of the square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module demonstrates the use of classes by
+creating a square class
+"""
+
+
+class Square(object):
+    """A class use to represent a square Object"""
+    def __init__(self, size):
+        """Initialize a square object"""
+
+        self._size = size
