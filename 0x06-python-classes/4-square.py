49f0c93e166b20da4e0786cbccea6af8acba35f6
Theocode12 <vchidubem9@gmail.com>
2022-06-21 23:56:29 +0100

    setter and getters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+This module demonstrates the use of classes by
+creating a square class
+"""
+
+
+class Square(object):
+    """A class use to represent a square Object"""
+
+    def __init__(self, size=0):
+        """Initialize a square object"""
+
+        self._Square__size = size
+
+    @property
+    def size(self):
+        """A getter function that retrieves a private variable"""
+
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        """A setter variable used to set a private variable"""
+
+        if (isinstance(value, int)) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self._Square__size = value
+
+    def area(self):
+        """Return the area of this square"""
+
+        return self._Square__size ** 2
