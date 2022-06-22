84a2bb03b04f96c53479d44e2ddfb8f133adf395
Theocode12 <vchidubem9@gmail.com>
2022-06-22 07:46:19 +0100

    position of square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,41 +12,42 @@ class Square(object):
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a square object"""
 
-        self._Square__size = size
         self._Square__position = position
+        self._Square__size = size
 
     @property
-    def position(self):
+    def _Square__position(self):
         """Getter for position of square"""
 
-        return self._Square__position
+        return self.__Square__position
 
-    @position.setter
-    def position(self, value):
+    @_Square__position.setter
+    def _Square__position(self, value):
         """Setter for position of square"""
 
         if (len(value) == 2 and value[0] >= 0 and value[1] >= 0
             and isinstance(value, tuple)
             and isinstance(value[0], int)
                 and isinstance(value[1], int)):
-            self._Square__position = value
+            self.__Square__position = value
+            return
         raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
-    def size(self):
+    def _Square__size(self):
         """A getter function that retrieves a private variable"""
 
-        return self._Square__size
+        return self.__Square__size
 
-    @size.setter
-    def size(self, value):
+    @_Square__size.setter
+    def _Square__size(self, value):
         """A setter variable used to set a private variable"""
 
         if (isinstance(value, int)) is False:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self._Square__size = value
+        self.__Square__size = value
 
     def area(self):
         """Return the area of this square"""
