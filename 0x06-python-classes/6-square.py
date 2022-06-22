97590fadad3c8a574e555545ed7ddb63e9b27f8b
Theocode12 <vchidubem9@gmail.com>
2022-06-22 01:45:02 +0100

    coordinate squares

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -25,7 +25,10 @@ class Square(object):
     def position(self, value):
         """Setter for position of square"""
 
-        if len(value) != 2 or value[0] < 0 or value[1] < 0:
+        if (len(value) == 2 and value[0] >= 0 and value[1] >= 0
+            and isinstance(value, tuple) is True
+            and isinstance(value[1], int) is True
+                and isinstance(value[2], int) is True):
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self._Square__position = value
