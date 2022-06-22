96895e0fb3c39d3d290ed5a5da3a21cf3f09cee0
Theocode12 <vchidubem9@gmail.com>
2022-06-22 08:03:41 +0100

    position of square class

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -12,55 +12,55 @@ class Square(object):
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a square object"""
 
-        self._Square__position = position
-        self._Square__size = size
+        self.size = size
+        self.position = position
 
     @property
-    def _Square__position(self):
+    def position(self):
         """Getter for position of square"""
 
-        return self.__Square__position
+        return self._position
 
-    @_Square__position.setter
-    def _Square__position(self, value):
+    @position.setter
+    def position(self, value):
         """Setter for position of square"""
 
         if (len(value) == 2 and value[0] >= 0 and value[1] >= 0
             and isinstance(value, tuple)
             and isinstance(value[0], int)
                 and isinstance(value[1], int)):
-            self.__Square__position = value
+            self._position = value
             return
         raise TypeError("position must be a tuple of 2 positive integers")
 
     @property
-    def _Square__size(self):
+    def size(self):
         """A getter function that retrieves a private variable"""
 
-        return self.__Square__size
+        return self._size
 
-    @_Square__size.setter
-    def _Square__size(self, value):
+    @size.setter
+    def size(self, value):
         """A setter variable used to set a private variable"""
 
         if (isinstance(value, int)) is False:
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
-        self.__Square__size = value
+        self._size = value
 
     def area(self):
         """Return the area of this square"""
 
-        return self._Square__size ** 2
+        return self.size ** 2
 
     def my_print(self):
         """Prints a square"""
-        if self._Square__size == 0:
+        if self.size == 0:
             print()
             return
-        for i in range(self._Square__position[1]):
+        for i in range(self.position[1]):
             print()
-        for i in range(self._Square__size):
-            print(" "*self._Square__position[0], end="")
-            print('#'*self._Square__size)
+        for i in range(self.size):
+            print(" "*self.position[0], end="")
+            print('#'*self.size)
