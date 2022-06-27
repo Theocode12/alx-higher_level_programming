8400e4eb7487df7e3f8ae5c497822ee2a149a167
Theocode12 <vchidubem9@gmail.com>
2022-06-27 17:31:01 +0100

    More on Python classes and objects

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -3,6 +3,9 @@
 square"""
 
 
+from typing import Type
+
+
 class Rectangle(object):
     """Represent a class rectangle"""
 
@@ -67,9 +70,9 @@ class Rectangle(object):
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) is not Rectangle:
-            print("rect_1 must be an instance of Rectangle")
+            raise TypeError("rect_1 must be an instance of Rectangle")
         if type(rect_2) is not Rectangle:
-            print("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
         else:
