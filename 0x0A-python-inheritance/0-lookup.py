55babd8b0e9de9c2d6a6808e434cd8307ce75f25
Valentine <vchidubem9@gmail.com>
2022-07-04 16:03:33 +0100

    lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""A module to check for available attributes and methods"""
+
+
 def lookup(obj):
+    """A function that returns a list of availble attributes and methods
+    of an object"""
+
     return list(dir(obj))
