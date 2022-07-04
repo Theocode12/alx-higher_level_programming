3fcf7300d8718c41a384cb6576e5e8f7d56d5bf4
Valentine <vchidubem9@gmail.com>
2022-07-05 00:19:48 +0100

    square object

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""A module for square object"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""Importing Rectangle class"""
+
+
+class Square(Rectangle):
+    """A square object"""
+
+    def __init__(self, size):
+        """Constructor for square object"""
+
+        Rectangle.integer_validator(self, "size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        """Area of square object"""
+
+        return self.__size ** 2
