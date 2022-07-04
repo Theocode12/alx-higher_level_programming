b3d8cbdb219a5680ae505b24fc9959748b9a1344
Valentine <vchidubem9@gmail.com>
2022-07-04 16:40:50 +0100

    kind of class

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,7 @@
 """A module that checks if an object is an instance of a class
 that inherited from another class"""
 
+
 def is_kind_of_class(obj, a_class):
     """Checks if an obj is a kind of class"""
 
