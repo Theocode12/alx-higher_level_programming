1bbbfe60359b2ae6515d2cc11a2a24b080f5de81
Valentine <vchidubem9@gmail.com>
2022-07-05 04:28:32 +0100

    setattr

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""set atrribute module"""
+
+def add_attribute(self, name, value):
+    """add attribute function"""
+
+    if hasattr(self, '__dict__'):
+        setattr(self, name, value)
+    else:
+        raise TypeError("can't add new attribute")
