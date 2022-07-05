29cb1404e779047ca8a62e41e8eda7ec750fa799
Valentine <vchidubem9@gmail.com>
2022-07-05 02:51:08 +0100

    list superclass

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module inherits the list object and uses it
+to sort list items"""
+
+class MyList(list):
+    """This class add more functionality to the list object"""
+
+    def print_sorted(self):
+        print(sorted(self))
