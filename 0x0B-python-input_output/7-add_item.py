63406baaaee280a675aa59689e7138bdde97abfb
Valentine <vchidubem9@gmail.com>
2022-07-06 18:23:02 +0100

    add_item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that adds all arguements in to a python list."""
+
+import sys
+import os
+"""Importing the sys and os module"""
+
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+"""Importing the save_to_json module"""
+
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""Importing the load_from_json module"""
+
+
+args = sys.argv
+if os.path.exists("add_item.json"):
+    text = load_from_json_file("add_item.json")
+if len(args) > 1:
+    args = args[1:]
+else:
+    args = []
+if len(text) == 0:
+    text = []
+text.extend(args)
+save_to_json_file(text, "add_item.json")
