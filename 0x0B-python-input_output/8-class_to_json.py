6e5e738aa623070b073463a6cea0f56287857e3c
Valentine <vchidubem9@gmail.com>
2022-07-06 19:44:45 +0100

    class_to_json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+A module that return a dictionary of a class
+"""
+
+def class_to_json(obj):
+    my_dict = obj.__dict__
+    for key in my_dict:
+        if key[-2:] == "__":
+            del my_dict[key]
+    return my_dict
