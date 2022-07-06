03488319c6710d5e0c5c55ea3243a147c73f67e8
Valentine <vchidubem9@gmail.com>
2022-07-06 20:00:31 +0100

    student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+A module that retrives dictionary rep of student
+"""
+
+
+class Student(object):
+    """
+    A student class
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    
+    def to_json(self):
+        stu_dict = self.__dict__.copy()
+        for key in stu_dict:
+            if key[-2:] == "__":
+                del stu_dict[key]
+        return stu_dict
