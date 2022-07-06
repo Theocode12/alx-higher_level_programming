068157da7a8f8eba3a9312b9161de087831ba6fe
Valentine <vchidubem9@gmail.com>
2022-07-06 20:49:19 +0100

    encoding correction

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,5 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Save to a json file."""
 
-    with open(filename, 'w') as j_file:
+    with open(filename, 'w', encoding='utf-8') as j_file:
         json.dump(my_obj, j_file)
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,6 +8,6 @@ import json
 def load_from_json_file(filename):
     """loads py object from json file"""
 
-    with open(filename, 'r') as j_obj:
+    with open(filename, 'r', encoding='utf-8') as j_obj:
         py_obj = json.load(j_obj)
     return py_obj
