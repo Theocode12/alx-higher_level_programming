a4f9218e02ccefc75194ade0c798dc5ca8374b7c
Valentine <vchidubem9@gmail.com>
2022-07-07 10:49:37 +0100

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,6 +6,7 @@ save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
+
 args = argv
 if len(args) > 1:
     args = args[1:]
@@ -13,6 +14,7 @@ else:
     args = []
 try:
     text = load_from_json_file(filename)
+
 except Exception:
     text = []
 
