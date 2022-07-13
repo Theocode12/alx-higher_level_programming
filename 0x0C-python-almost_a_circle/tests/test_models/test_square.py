6518db7ac69d728eda49e684da20b1d628d29f15
Valentine <vchidubem9@gmail.com>
2022-07-13 05:42:35 +0100

    update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import unittest
+import json
+import inspect
+import pep8
+import os
+from unittest.mock import patch
+from io import StringIO
+from models import square
+
+Square = square.Square
