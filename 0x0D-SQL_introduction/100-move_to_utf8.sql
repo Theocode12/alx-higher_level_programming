2fab100c1c5d31e81970f39666748924b3009c65
root <vchidubem9@gmail.com>
2022-08-10 02:35:59 -0700

    move to utf-8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,8 +4,9 @@
 USE hbtn_0c_0;
 ALTER DATABASE
 	hbtn_0c_0
-	CHARACTER SET = utf8mb4
+	CHARACTER SET utf8mb4
 	COLLATE utf8mb4_unicode_ci;
 ALTER TABLE
-	first_table	
+	first_table
+	CONVERT TO CHARACTER SET utf8mb4	
 	COLLATE utf8mb4_unicode_ci;
