d6c6b55b2e1e0f80ee61d194e21403254a7b507c
theocode12 <vchidubem9@gmail.com>
2022-08-11 21:43:42 +0100

    Task 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,5 @@ SELECT tv_genres.name
     ON tv_genres.id = tv_show_genres.genre_id
     JOIN tv_shows
     ON tv_show_genres.show_id = tv_shows.id
-    WHERE tv_shows.title = 'Dexter';
+    WHERE tv_shows.title = 'Dexter'
+    ORDER BY tv_genres.name;
