1626cc10961135cf98c553d2271a4e53b4bf71a8
theocode12 <vchidubem9@gmail.com>
2022-08-11 22:49:33 +0100

    Task 100

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -11,4 +11,4 @@ FROM tv_genres
                                         ON tv_shows.id = tv_show_genres.show_id
                                         WHERE tv_shows.title = 'Dexter'
                                 )
-    ORDER BY tv_genres.name; 
+    ORDER BY tv_genres.name;
