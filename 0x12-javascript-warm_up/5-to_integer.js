75fa0bb626bd642e1abb41a9905f8440a04e563e
theocode12 <vchidubem9@gmail.com>
2022-09-06 08:54:37 +0100

    Warmup

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,4 +4,4 @@ require('process');
 const argv = process.argv;
 
 const number = parseInt(argv[2]);
-if (Number.isNaN(number)) { console.log('Not a number'); } else { console.log("My number:", number); }
+if (Number.isNaN(number)) { console.log('Not a number'); } else { console.log('My number:', number); }
