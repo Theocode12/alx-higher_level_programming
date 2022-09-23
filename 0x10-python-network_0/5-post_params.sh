d13f06dbda8149691ac8ac679f8e056cd7b30392
theocode12 <vchidubem9@gmail.com>
2022-09-23 04:44:14 +0100

    5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL and displays the body of the response
-curl -d "email=test@gmail%2Ecom&subject=I2Bwill2Balways2Bbe2Bhere2Bfor2BPLD" -X POST $1
+curl -ds "email=test@gmail%2Ecom&subject=I2Bwill2Balways2Bbe2Bhere2Bfor2BPLD" -X POST $1
