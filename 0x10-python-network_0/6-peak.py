78b511b7559bf05d344402fa6e369b99bc6f1fec
theocode12 <vchidubem9@gmail.com>
2022-09-23 11:57:37 +0100

    peak TI

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module that calculates a peak in a list of unsorted integers"""
+
+def find_peak(list_int):
+    """
+    A function that finds a peak in a list of unsorted integers
+    """
+
+    for i in range(len(list_int)):
+        if i > 0 and list_int[i] < list_int[i - 1]:
+            return list_int[i - 1]
+    if list_int:
+        return list_int[0]
+
