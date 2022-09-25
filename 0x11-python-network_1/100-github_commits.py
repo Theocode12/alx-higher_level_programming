0791122b129e97f5120fdd959d4ea683215c8b81
theocode12 <vchidubem9@gmail.com>
2022-09-25 11:51:18 +0100

    github commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
 """
-Please list 10 commits (from the most recent to oldest)
-of the repository “rails” by the user “rails”
+ Python script that fetches https://alx-intranet.hbtn.io/status
 """
-
-from sys import argv
-import requests
-
-
-def main():
-    """Main function"""
-
-    usr_name = argv[1]
-    repo_name = argv[2]
-    url = f"https://api.github.com/repos/{usr_name}/{repo_name}/commits"
-    commits = requests.get(url).json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
 
 
 if __name__ == "__main__":
-    main()
+    import requests
+    import sys
+
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+
+    url = f'https://api.github.com/repos/{owner}/{repo}/commits'
+    req = requests.get(url)
+    req = req.json()
+
+    for x in req[:10]:
+        print(x.get('sha'), end=': ')
+        print(x.get('commit').get('author').get('name'))
