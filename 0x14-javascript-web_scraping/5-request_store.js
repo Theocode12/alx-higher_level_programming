4f284b58ddfe42f8bc5c524f71829380dd282ccd
Theocode12 <vchidubem9@gmail.com>
2022-10-28 00:55:42 +0100

    5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const fs = require('fs');
 const argv = require('process').argv.slice(2);
-url = argv[0];
-filename = argv[1];
+const url = argv[0];
+const filename = argv[1];
 
 request(url).pipe(fs.createWriteStream(filename));
