fdd80abac34a81ad80ef1531fc85c8c72bc805b3
Theocode12 <vchidubem9@gmail.com>
2022-10-28 23:39:26 +0100

    advanced task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -3,12 +3,14 @@
 const request = require('request');
 const argv = require('process').argv.slice(2);
 
-let url = "https://swapi-api.hbtn.io/api/films/" + argv[0] + "/";
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[0] + '/';
 request.get(url, (err, resp, body) => {
-	charactersList = JSON.parse(body).characters;
-	for (let characterUrl of charactersList){
-		request.get(characterUrl, (err, resp, body) => {
-			console.log(JSON.parse(body).name);
-		})
-	}
+  if (err) { console.error(err); }
+  const charactersList = JSON.parse(body).characters;
+  for (const characterUrl of charactersList) {
+    request.get(characterUrl, (err, resp, body) => {
+      if (err) { console.error(err); }
+      console.log(JSON.parse(body).name);
+    });
+  }
 });
